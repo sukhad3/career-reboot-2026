244873e603255910ceecb9f6f42622c17ab7951d
Sukhdev <sukhad3@gmail.com>
2026-07-27 13:49:03 +0530

    Initialize Spring Boot project with Gradle

---
+++ week01-java21/src/main/java/com/sukhdev/reboot/career_reboot/CareerRebootApplication.java
@@ -0,0 +1,13 @@
+package com.sukhdev.reboot.career_reboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerRebootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerRebootApplication.class, args);
+	}
+
+}
---
+++ week01-java21/src/test/java/com/sukhdev/reboot/career_reboot/CareerRebootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sukhdev.reboot.career_reboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerRebootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
